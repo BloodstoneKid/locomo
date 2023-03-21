9911fa903ce95236b326cb75f38512a90520faa3
BloodstoneKid <andresperdomo737@gmail.com>
2023-03-21 22:49:18 +0100

    Prototipo de uso de semaforos

--- lomo.c
+++ lomo.c
@@ -20,7 +20,7 @@ struct trenes{
     int pid;
 };
 
-int semaforo[2],buzon;
+int semaforo[21],buzon;
 int nTrenes;
 struct trenes tren[100];
 
@@ -40,9 +40,10 @@ void errorHandler(){
 	}
 	
 void cierre(int signum){
-		semctl(semaforo[0],0,IPC_RMID);
-		semctl(semaforo[1],0,IPC_RMID);
-		msgctl(buzon,IPC_RMID,NULL);
+	for(int i=0; i<21; i++){
+		semctl(semaforo[i],0,IPC_RMID);
+	}
+	msgctl(buzon,IPC_RMID,NULL);
         for(int i=0; i<nTrenes; i++){
             kill(tren[i].pid,SIGTERM);
         }
@@ -53,8 +54,43 @@ void mata(){
         for(int i=0; i<nTrenes; i++){
             kill(tren[i].pid,SIGTERM);
         }
-        semctl(semaforo[0],0,IPC_RMID);
+        for(int i=0; i<21; i++){
+		semctl(semaforo[i],0,IPC_RMID);
+	}
         msgctl(buzon,IPC_RMID,NULL);
+}
+
+int localiza(int x, int y){
+	switch(x){
+		case 0: if(y==0) return 0;
+			else if(y==9) return 11;
+			else if(y==12) return 13;
+			break;
+		case 16:if(y==0) return 1;
+			else if(y==4) return 5;
+			else if(y==7) return 6;
+			else if(y==9) return 12;
+			else if(y==12) return 14;
+			else if(y==16) return 17;
+			break;
+		case 36:if(y==0) return 2;
+			else if(y==7) return 7;
+			else if(y==12) return 15;
+			else if(y==16) return 18;
+			break;
+		case 54:if(y==0) return 3;
+			else if(y==7) return 8;
+			else if(y==12) return 16;
+			else if(y==16) return 19;
+			break;
+		case 68:if(y==0) return 4;
+			else if(y==7) return 9;
+			else if(y==16) return 20;
+			break;
+		case 74: return 10;
+			break;
+		default: return -1;
+	}
 }
 
 int main(int argc, char *argv[]){
@@ -71,10 +107,12 @@ int main(int argc, char *argv[]){
                 errorHandler();
             }
 
-			key_t clave = ftok(argv[0],'G');
-			semaforo[0] = semget(clave,1,IPC_CREAT | 0600);
-            if(semaforo[0]==-1) perror("crear semaforo");
-			buzon = msgget(clave,IPC_CREAT | 0600);
+		key_t clave = ftok(argv[0],'G');
+		for(int i=0; i<21; i++){
+			semaforo[i] = semget(clave,1,IPC_CREAT | 0600);
+            		if(semaforo[i]==-1) perror("crear semaforo");
+		}
+		buzon = msgget(clave,IPC_CREAT | 0600);
             if(buzon==-1) perror("crear buzon");
             
 			
@@ -109,6 +147,8 @@ int main(int argc, char *argv[]){
                 if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),IPC_NOWAIT)!=0) perror("buzon snd 4");
                 if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_RESPAVANCETREN0+msg.tren,1)==0) perror("buzon rcv 4");
 
+		/*int numsem = localiza(msg.x,msg.y);
+		if(numsem!=-1) wait(&semaforo[numsem]);*/
                 LOMO_espera(oldY,msg.y);
                 sleep(1);
                 }

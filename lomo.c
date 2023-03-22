4c8c1e32b600b9713c77391edf80dad96ccd62fa
Ruipi <juliruipieco@gmail.com>
2023-03-22 15:30:40 +0100

    prototipo de algoritmo, no funciona wait y signal por motivo desconocido

--- lomo.c
+++ lomo.c
@@ -11,18 +11,21 @@
 #include "lomo.h"
 
 #define TIPO_GUARDAPID 400
+#define TIPO_SALIENDO 500
+#define NUMSEMS 3
 
 #define LOGIN1 "i6687129"
 #define LOGIN2 "i1080834"
 
-struct trenes{
-    int ntren;
-    int pid;
-};
 
-int semaforo[21],buzon;
-int nTrenes;
-struct trenes tren[100];
+int semaforo[NUMSEMS],buzon;
+int nTrenes, pids[100], idTren;
+struct sembuf sops;
+union semun{
+    int val;
+    struct semid_ds *buf;
+    __u_short *array;
+};
 
 bool esNumero(char number[]){
     		int i = 0;
@@ -40,63 +43,97 @@ void errorHandler(){
 	}
 	
 void cierre(int signum){
-	for(int i=0; i<21; i++){
+    LOMO_fin();
+	for(int i=0; i<NUMSEMS; i++){
 		semctl(semaforo[i],0,IPC_RMID);
 	}
 	msgctl(buzon,IPC_RMID,NULL);
-        for(int i=0; i<nTrenes; i++){
-            kill(tren[i].pid,SIGTERM);
-        }
-		printf("Cerrados");
-	}	
+    for(int i=0; i<nTrenes; i++){
+        kill(pids[i],SIGTERM);
+    }
+    printf("Cerrados");
+}	
 
 void mata(){
-        for(int i=0; i<nTrenes; i++){
-            kill(tren[i].pid,SIGTERM);
-        }
-        for(int i=0; i<21; i++){
-		semctl(semaforo[i],0,IPC_RMID);
+    for(int i=0; i<nTrenes; i++){
+        kill(pids[i],SIGTERM);
+    }
+    for(int i=0; i<NUMSEMS; i++){
+        semctl(semaforo[i],0,IPC_RMID);
 	}
-        msgctl(buzon,IPC_RMID,NULL);
+    msgctl(buzon,IPC_RMID,NULL);
 }
 
 int localiza(int x, int y){
 	switch(x){
-		case 0: if(y==0) return 0;
-			else if(y==9) return 11;
-			else if(y==12) return 13;
+		case 0: 
+            if(y==0) return 0;
+            else if(y==9) return 11;
+            else if(y==12) return 13;
 			break;
-		case 16:if(y==0) return 1;
-			else if(y==4) return 5;
-			else if(y==7) return 6;
-			else if(y==9) return 12;
-			else if(y==12) return 14;
-			else if(y==16) return 17;
+		case 16:
+            if(y==0) return 1;
+            else if(y==4) return 5;
+            else if(y==7) return 6;
+            else if(y==9) return 12;
+            else if(y==12) return 14;
+            else if(y==16) return 17;
+            break;
+		case 36:
+            if(y==0) return 2;
+            else if(y==7) return 7;
+            else if(y==12) return 15;
+            else if(y==16) return 18;
+            break;
+        case 54:
+            if(y==0) return 3;
+            else if(y==7) return 8;
+            else if(y==12) return 16;
+            else if(y==16) return 19;
+            break;
+        case 68:
+            if(y==0) return 4;
+            else if(y==7) return 9;
+            else if(y==16) return 20;
 			break;
-		case 36:if(y==0) return 2;
-			else if(y==7) return 7;
-			else if(y==12) return 15;
-			else if(y==16) return 18;
-			break;
-		case 54:if(y==0) return 3;
-			else if(y==7) return 8;
-			else if(y==12) return 16;
-			else if(y==16) return 19;
-			break;
-		case 68:if(y==0) return 4;
-			else if(y==7) return 9;
-			else if(y==16) return 20;
-			break;
-		case 74: return 10;
+		case 74: 
+            return 10;
 			break;
 		default: return -1;
 	}
 }
 
+
+int w(int semid, int semnum, int n){
+    sops.sem_op=-n;
+    sops.sem_flg=0;
+    sops.sem_num=semnum;
+    int waiting = semop(semid,&sops,1); 
+    if(waiting==-1){
+        perror("Opercion wait al semaforo error");
+        return -1;
+    }
+
+    return waiting;
+}
+
+int s(int semid, int semnum, int n){
+    sops.sem_op=n;
+    sops.sem_flg=0;
+    sops.sem_num=semnum;
+    int waiting = semop(semid,&sops,1); 
+    if(waiting==-1){
+        perror("Opercion signal al semaforo error");
+        return -1;
+    }
+
+    return waiting;
+}
+
 int main(int argc, char *argv[]){
 	struct sigaction nuevo;
 	nuevo.sa_handler = cierre;
-	sigaction(SIGINT,&nuevo,NULL);
+	if(sigaction(SIGINT,&nuevo,NULL)==-1) perror("sigaction");
 
 	if (argc==2 || argc==3){
 		if(!strcmp(argv[1],"--mapa")){
@@ -107,52 +144,74 @@ int main(int argc, char *argv[]){
                 errorHandler();
             }
 
-		key_t clave = ftok(argv[0],'G');
-		for(int i=0; i<21; i++){
-			semaforo[i] = semget(clave,1,IPC_CREAT | 0600);
-            		if(semaforo[i]==-1) perror("crear semaforo");
-		}
-		buzon = msgget(clave,IPC_CREAT | 0600);
+            semaforo[0] = semget(IPC_PRIVATE,1,IPC_CREAT | 0600);
+            semaforo[2] = semget(IPC_PRIVATE,1,IPC_CREAT | 0600);
+            semaforo[1] = semget(IPC_PRIVATE,21,IPC_CREAT | 0600);
+            if(semaforo[0]==-1||semaforo[2]==-1||semaforo[1]==-1) perror("crear semaforo");
+            for(int i=0; i<21; i++){
+                semctl(semaforo[1],i,SETVAL,1);
+            }
+            semctl(semaforo[2],1,SETVAL,1);
+
+            buzon = msgget(IPC_PRIVATE,IPC_CREAT | 0600);
             if(buzon==-1) perror("crear buzon");
-            
-			
-			LOMO_inicio(*argv[1],semaforo[0],buzon,LOGIN1,LOGIN2);
-	struct mensaje msg;
-            for(int i=0; i<nTrenes; i++){
-            if(!fork()){
-                msg.tipo=TIPO_TRENNUEVO;
-                if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),IPC_NOWAIT)!=0) perror("buzon snd 1");
-                if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_RESPTRENNUEVO,1)==0) perror("buzon rcv 1");
                 
-                tren[i].ntren=msg.tren;
-                msg.tipo=TIPO_GUARDAPID;
-                msg.tren=getpid();
-                if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),IPC_NOWAIT)!=0) perror("buzon snd 2");
+                
+            LOMO_inicio(*argv[1],semaforo[0],buzon,LOGIN1,LOGIN2);
 
+            struct mensaje msg;
+            for(int i=0; i<nTrenes; i++){
+                switch(fork()){
+                    case 0:
+                        msg.tipo=TIPO_TRENNUEVO;
+                        if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),0)==-1) perror("buzon snd 1");
+                        if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_RESPTRENNUEVO,1)==-1) perror("buzon rcv 1");
+                        
+                        idTren=msg.tren;
+                        msg.tipo=TIPO_GUARDAPID;
+                        msg.tren=getpid();
+                        if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),0)==-1) perror("buzon snd 2");
+                        break;
 
-            }else{
-                if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_GUARDAPID,1)==0) perror("buzon rcv 2");
-                tren[i].pid=msg.tren;
+                    case -1:
+                        perror("fork");
+
+                    default:
+                        if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_GUARDAPID,1)==-1) perror("buzon rcv 2");
+                        pids[i]=msg.tren;
+                        break;
+                }
             }
-            }
-                while(1){
-                for(int i=0; i<nTrenes; i++){
+
+            int nuevoX, nuevoY, libreX=-1, libreY=-1, viejoLX=0, viejoLY=0;
+            while(1){
+                viejoLY=libreY; viejoLX=libreX;
                 msg.tipo=TIPO_PETAVANCE;
-                msg.tren = tren[i].ntren;
-                if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),IPC_NOWAIT)!=0) perror("buzon snd 3");
-                if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_RESPPETAVANCETREN0+msg.tren,1)==0) perror("buzon rcv 3");
-                int oldY = msg.y;
+                msg.tren = idTren;
+                if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),0)==-1) perror("buzon snd 3");
+                if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_RESPPETAVANCETREN0+msg.tren,1)==-1) perror("buzon rcv 3");
+                
+                nuevoX = msg.x; nuevoY = msg.y;
+
+                int numsem = localiza(nuevoX,nuevoY);
+                if(numsem!=-1) w(semaforo[1], numsem, 1);
+
+                if((libreX==-1 || libreY==-1) && (viejoLX!=-1||viejoLY!=-1)){
+                    w(semaforo[2],0,1);
+                }else if((libreX!=-1 || libreY!=-1) && (viejoLX==-1||viejoLY==-1)){
+                    s(semaforo[2],0,1);
+                }
 
                 msg.tipo=TIPO_AVANCE;
                 if(msgsnd(buzon,&msg,sizeof(msg)-sizeof(long),IPC_NOWAIT)!=0) perror("buzon snd 4");
                 if(msgrcv(buzon,&msg,sizeof(msg)-sizeof(long),TIPO_RESPAVANCETREN0+msg.tren,1)==0) perror("buzon rcv 4");
-
-		/*int numsem = localiza(msg.x,msg.y);
-		if(numsem!=-1) wait(&semaforo[numsem]);*/
-                LOMO_espera(oldY,msg.y);
-                sleep(1);
+                libreX = msg.x; libreY = msg.y;
+                if(localiza(libreX, libreY)==-1 && numsem!=-1){
+                    s(semaforo[1],numsem,1);
                 }
-                }
+                
+                LOMO_espera(nuevoY,libreY);
+            }
             LOMO_fin();
 		}else{
 			errorHandler();
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 lomo: lomo.c liblomo.a
 	gcc lomo.c liblomo.a -o lomo -m32
 
+
 clean: 
 	rm -r *.o
